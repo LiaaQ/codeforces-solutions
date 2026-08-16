042225187688f326a720a4d4fe2fc6ef2a4f6f81
Natálie Teplická <teplicka.natalie@gmail.com>
2026-08-16 23:45:31 +0100

    Create 2256B.cpp

---
+++ Problems/1000/2256B/2256B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int amt_problems, num_chars;
+    string input;
+    cin >> amt_problems;
+
+    for(int i = 0; i < amt_problems; i++) {
+        cin >> num_chars >> input;
+
+        if(num_chars%2==0) {
+
+        }
+    }
+}
