9a8b9a9d57eed0a0790289e651506cdfdc23357b
Natálie Teplická <teplicka.natalie@gmail.com>
2026-08-25 19:09:32 +0100

    1669E

--- Contests/Div.4/784/E.cpp
+++ Contests/Div.4/784/E.cpp
@@ -1,6 +1,48 @@
 #include<iostream>
+#include<map>
+#include<string>
 using namespace std;
+
+string v[100000];
+char characters[11] = {'a','b','c','d','e','f','g','h','i','j','k'};
+
+void add_counts(string except, map<string, int> &m) {
+    string s = string() + except[0] + 'x';
+    for(char &x : characters) {
+        s[1] = x;
+        if(s!=except) m[s]++;
+    }
+
+    s = string() + 'x' + except[1];
+
+    for(char &x : characters) {
+        s[0] = x;
+        if(s!=except) m[s]++;
+    }
+    
+}
+
 int main()
 {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+        long long ans = 0;
+        map<string, int> m;
+
+        for(int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+
+        for(int i = n - 1; i >= 0; i--) {
+            add_counts(v[i], m);
+            ans+=m[v[i]];
+        }
+
+        cout << ans << endl;
+    }
     return 0;
 }
